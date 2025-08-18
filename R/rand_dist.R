61e2d41400b3cc0bf98ea8baace22eb9de11c544
Vera Kvisgaard <verakvisgaard@eduroam-193-157-255-233.wlan.uio.no>
2025-08-18 15:23:58 +0200

    add possibility of user-specified function for simulating local distributions

--- R/rand_dist.R
+++ R/rand_dist.R
@@ -6,7 +6,9 @@
 #' such objects.
 #'
 #' @param dag (integer matrix) an adjacency matrix
-#' @param type (character) type of distribution
+#' @param type (character) type of distribution. Ignored if `rand_local_dist` is not `NULL`.
+#' @param rand_local_dist (function) a function with arguments `j, parentnodes, args` that generates a random distribution.
+#'  If `NULL` (default), and `type == categorical` then the function `rand::rand_cpt` is used.
 #' @param ... additional arguments for the function producing each local distribution.
 #'  See [rand_cpt()] for categorical distributions.
 #'
@@ -20,7 +22,7 @@
 #' dag[upper.tri(dag)] <- 1
 #' rand_dist(dag, "cat", nlev = rep(3, 3))
 #'
-rand_dist <- function(dag, type = "cat", ...) {
+rand_dist <- function(dag, type = "cat", rand_local_dist = NULL, ...) {
   type <- match.arg(type, c("categorical"))
 
   n <- ncol(dag)
@@ -29,8 +31,8 @@ rand_dist <- function(dag, type = "cat", ...) {
 
   # specify default params and a function to draw local distribution
   args <- list(...)
-  if (type == "categorical") {
-    draw_dist <- function(j, parentnodes, args) {
+  if (is.null(rand_local_dist) && type == "categorical") {
+    rand_local_dist <- function(j, parentnodes, args) {
       if (is.null(args$nlev)) stop("Add argument `nlev` specifying the cardinality of each node.")
       args$nlev  <- args$nlev[c(j, parentnodes)]
       args$scope <- varnames[c(j, parentnodes)]
@@ -41,7 +43,7 @@ rand_dist <- function(dag, type = "cat", ...) {
   dist <- setNames(vector("list", n), varnames)
   for (j in seqn) {
     parentnodes <- seqn[dag[, j] == 1]
-    dist[[j]] <- draw_dist(j, parentnodes, args)
+    dist[[j]] <- rand_local_dist(j, parentnodes, args)
   }
   dist
 }
